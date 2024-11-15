215a107b3b8f4555bf8fda4ca06b078f3d07f68e
uzzalcse <uzzal.cse42@gmail.com>
2024-11-15 18:33:03 +0600

    fetch data from the api and show in browser

---
+++ app/hotel-details/[slug]/[id]/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header"
+import ShareAndSave from "@/components/ShareAndSave";
+
+
+export default function Home() {
+  return (
+    <div>
+    <Header/>
+    <ShareAndSave/>
+    </div>
+  );
+}
